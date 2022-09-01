2a53e99edf205b225a0926a3ac94a6a1b0e0ac47
= <masixolematolweni.1@gmail.com>
2022-09-02 01:13:21 +0200

    environment key update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: true,
   superbaseUrl: 'https://dcuozczkflrsjdekvokc.supabase.co',
   sperbaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjdW96Y3prZmxyc2pkZWt2b2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE2MDM0ODQsImV4cCI6MTk3NzE3OTQ4NH0.qqkGfOk8PxWYoq5P7pR8RqeR-2kkj01wUlgZGwpiN2g',
-  baseUrl: 'https://api.github.com/repos/1-grid/GitIntegration/issues',
-  auth: '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',
+  baseUrl: 'https://api.github.com/repos/Matolweni95/age-calc/issues',
+  auth: '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',
   owner: '1-grid',
   repo: 'GitIntegration'
 };
